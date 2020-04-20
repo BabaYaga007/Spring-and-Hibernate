106d9642414c42973f3f57cb2544f2c02cf9c188
Achint Srivastava <32853258+BabaYaga007@users.noreply.github.com>
2020-04-20 17:51:00 +0530

    Course Completion

---
+++ 24-spring-data-rest-cruddemo/src/test/java/awesome/springboot/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package awesome.springboot.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ 32-thymeleafdemo-employee-list/src/main/java/awesome/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -0,0 +1,63 @@
+package awesome.springboot.thymeleafdemo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import awesome.springboot.thymeleafdemo.model.Employee;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+
+	// load employee data
+	
+	private List<Employee> theEmployees;
+	
+	@PostConstruct
+	private void loadData() {
+		
+		//create employees
+		Employee emp1 = new Employee(1, "Red", "John", "red@john.com");
+		Employee emp2 = new Employee(2, "Mary", "Jane", "mary@jane.com");
+		Employee emp3 = new Employee(3, "Baba", "Yaga", "baba@yaga.com");
+		
+		//create the list 
+		theEmployees = new ArrayList<>();
+		
+		//add to the list
+		theEmployees.add(emp1);
+		theEmployees.add(emp2);
+		theEmployees.add(emp3);
+		
+		
+		
+	}
+	
+	
+	// add mapping for "/list"
+	@GetMapping("/list")
+	public String listEmployees(Model theModel) {
+		
+		// add to the spring model 
+		theModel.addAttribute("employees", theEmployees);
+		
+		return "list-employees";
+		
+	}
+	
+
+	
+}
+
+
+
+
+
+
