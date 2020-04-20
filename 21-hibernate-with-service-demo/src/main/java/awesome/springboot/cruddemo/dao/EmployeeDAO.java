59233e5440d512feff8224002bb92a83e7972393
Achint Srivastava <32853258+BabaYaga007@users.noreply.github.com>
2020-04-20 17:46:29 +0530

    Course Completion

---
+++ 21-hibernate-with-service-demo/src/main/java/awesome/springboot/cruddemo/dao/EmployeeDAO.java
@@ -0,0 +1,17 @@
+package awesome.springboot.cruddemo.dao;
+
+import java.util.List;
+
+import awesome.springboot.cruddemo.entity.Employee;
+
+public interface EmployeeDAO {
+	
+	public List<Employee> findAll();
+	
+	public Employee findById(int theId);
+	
+	public void save(Employee theEmployee);
+	
+	public void deleteById(int theId);
+
+}
